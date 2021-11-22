d3cfb0f718cac3578bc9b16fde41842d36d3b3fb
KUMAR7BITTU <94733746+KUMAR7BITTU@users.noreply.github.com>
2021-11-22 18:42:31 +0530

    Add files via upload

---
+++ pattern printing triangle shape type 3 using number (b).cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+    int n,i;
+    printf("\nEnter number of rows");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        printf("\n");
+        i=1;
+        while(i<=n)
+        {
+            printf("%d",i);
+            i++;
+        }
+        n=n-1;
+    }
+    return 0;
+}
